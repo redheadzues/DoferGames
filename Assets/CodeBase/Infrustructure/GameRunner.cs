6b9f84f3724f6a8d57431cfbbe265e54eee7c1ef
redheadzues <48310838+redheadzues@users.noreply.github.com>
2023-03-07 12:56:21 +0300

    Create gameRunner

---
+++ Assets/CodeBase/Infrustructure/GameRunner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.CodeBase.Infrustructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        public GameBootstraper BootstraperPrefab;
+
+        private void Awake()
+        {
+            GameBootstraper bootstrapper = FindObjectOfType<GameBootstraper>();
+
+            if (bootstrapper == null)
+                Instantiate(BootstraperPrefab);
+
+        }
+    }
+}
